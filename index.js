eabf83d06660a4b47d9662ef02e6cdd14a74c855
zh123789 <69754670+zh123789@users.noreply.github.com>
2026-08-19 20:35:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const protoLoader = require('@grpc/proto-loader');
 const { WebSocket, createWebSocketStream } = require('ws');
 
 // ========================== 环境变量配置 ==========================
-const UUID = process.env.UUID || 'd1cf4b9c-3e57-085d-b34a-797fcf601381';
+const UUID = process.env.UUID || 'c73c8497-dfd7-42c6-973d-f2a4e6771216';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
